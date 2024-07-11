43f2cef768a5b2cb120f42d8b0fae4b4ffef0db8
paradisomartin <martinparadiso024@gmail.com>
2024-07-11 20:12:06 -0300

    Hello Spark

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@
 import { ChakraProvider, Box, Heading } from '@chakra-ui/react';
 
 function App() {
+  const spark = "⚡️ Spark! ⚡️";
+  
   return (
     <ChakraProvider>
       <Box p={4}>
-        <Heading>Welcome to My App</Heading>
+        <Heading>Welcome to {spark}</Heading>
       </Box>
     </ChakraProvider>
   );
